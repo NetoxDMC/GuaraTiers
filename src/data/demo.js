adfd0046c2b3b9b6ee88f297aa9ca60b061b3009
NetoxDMC <lelitovillalba9@gmail.com>
2026-08-13 14:45:46 -0300

    Update demo.js

--- src/data/demo.js
+++ src/data/demo.js
@@ -3,7 +3,7 @@ export const demoPlayers = [
   { username: 'PlayerA', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000002', registeredAt: '2026-07-16', trend: 1, featured: true, ranks: { sword: ['LT1','HT1'], crystal: ['HT2','HT2'], axe: ['LT1','LT1'] } },
   { username: 'CrystalGod', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000003', registeredAt: '2026-06-24', trend: 0, featured: true, ranks: { crystal: ['HT1','HT1'], sword: ['HT2','HT2'], uhc: ['LT2','LT2'] } },
   { username: 'SwordMain', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000004', registeredAt: '2026-08-02', trend: -1, ranks: { sword: ['HT1','HT1'], uhc: ['LT1','LT1'], sumo: ['HT3','HT3'] } },
-  { username: 'PvPMaster', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000005', registeredAt: '2026-07-29', trend: 4, ranks: { netherite-pot: ['HT1','HT1'], diamond-pot: ['HT2','HT2'], mace: ['LT1','LT1'] } },
+  { username: 'PvPMaster', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000005', registeredAt: '2026-07-29', trend: 4, ranks: { 'netherite-pot': ['HT1','HT1'], 'diamond-pot': ['HT2','HT2'], mace: ['LT1','LT1'] },
   { username: 'PlayerB', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000006', registeredAt: '2026-08-04', trend: -2, ranks: { sword: ['HT2','HT2'], crystal: ['LT2','LT2'], axe: ['HT3','HT3'], smp: ['HT4','HT4'] } },
   { username: 'AstraByte', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000007', registeredAt: '2026-08-05', trend: 2, ranks: { uhc: ['HT1','HT1'], sword: ['LT2','LT2'] } },
   { username: 'VoidRift', uuid: 'e2a1f0de-41b4-4d9d-a1a0-000000000008', registeredAt: '2026-08-07', trend: 0, ranks: { crystal: ['HT2','HT2'], mace: ['HT2','HT2'], smp: ['LT2','LT2'] } },
