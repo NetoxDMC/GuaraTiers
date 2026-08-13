ccf32ea64ee0d352458deef5879daabec9973438
NetoxDMC <lelitovillalba9@gmail.com>
2026-08-13 14:34:58 -0300

    Update build.mjs

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,7 +1,12 @@
-import { cp, mkdir, rm } from 'node:fs/promises';
+import { cp, mkdir, rm, stat } from 'node:fs/promises';
 
 await rm('dist', { recursive: true, force: true });
 await mkdir('dist', { recursive: true });
 await cp('index.html', 'dist/index.html');
 await cp('src', 'dist/src', { recursive: true });
-await cp('public', 'dist', { recursive: true });
+try {
+  await stat('public');
+  await cp('public', 'dist', { recursive: true });
+} catch (error) {
+  if (error.code !== 'ENOENT') throw error;
+}
